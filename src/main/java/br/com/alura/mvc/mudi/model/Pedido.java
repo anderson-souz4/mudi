a06521a7cad57f0be61eb7d0525d14ce8e6027e2
anderson-souz4 <andersondesouzalopes69@gmail.com>
2022-10-04 17:33:43 -0300

    Projeto da formação Spring da Alura. Aplicação de pedidos

---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,34 @@
+package br.com.alura.mvc.mudi.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nomeProduto;
+    @Column
+    private BigDecimal valorNegociado;
+    @Column
+    private LocalDate dataEntrega;
+    @Column
+    private String urlProduto;
+    @Column
+    private String urlImagem;
+    @Column
+    private String descricao;
+
+
+}
